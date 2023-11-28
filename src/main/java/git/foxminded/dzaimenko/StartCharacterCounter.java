2fc158cd4c5595f9586eff2b93160ecb3a427bd2
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-11-29 01:39:35 +0200

    Fixed .gitignore

--- src/main/java/git/foxminded/dzaimenko/StartCharacterCounter.java
+++ src/main/java/git/foxminded/dzaimenko/StartCharacterCounter.java
@@ -4,7 +4,7 @@ public class StartCharacterCounter {
 
     public static void main(String[] args) {
         CharacterCounter characterCounter = new CharacterCounter();
-        String originalString = "Java is a high-level, class-based, object-oriented programming language";
+        String originalString = "Javalanguage";
 
         characterCounter.showResult(originalString);
 
